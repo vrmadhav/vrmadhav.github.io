ba51bf978bb8eb63111bcee0df147c7c1ada7375
vrmadhav <vignesh.ramesh@amplitude.com>
2022-05-11 16:27:58 -0700

    Bug fix

--- demo/prototype.js
+++ demo/prototype.js
@@ -5,10 +5,12 @@ console.log("JS Prototype file loaded");
 //remove uneccesary width crap
 function removeWidth(element) {
 	element.style.removeProperty('width');
-	console.log("removed width");
 }
 
 issueDivs = [];
 issueDivs = document.getElementsByClassName("Box_overflow-visible__1awWb");
 console.log(issueDivs);
-issueDivs.forEach(removeWidth);
+for (const element of issueDivs) {
+    removeWidth(element);
+  	console.log("removed width");
+}
